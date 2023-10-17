3b11a751164fbcd242818a72cb6c8b22663190e0
jmmirza98 <janmuhammadmirza@gmail.com>
2023-10-17 23:32:53 +0500

    changes

--- resources/views/frontend/about_us/index.blade.php
+++ resources/views/frontend/about_us/index.blade.php
@@ -8,13 +8,10 @@
             <h2 class="section-title font-alt mb-50 mt-40 mb-sm-40">About Chapter 85</h2>
             <div class="section-text mb-60 mb-sm-40">
                 <div class="row">
-                    <div class="col-sm-6 pr-md-5">
+                    <div class="col-sm-12 pr-md-5">
                         <p style="color:#254345">At Chapter 85, we are more than just leather craftsmen; we are passionate
                             artisans dedicated to the art of transforming the finest leathers into exquisite, handcrafted
                             products.</p>
-                    </div>
-                    <div class="col-sm-6 pl-md-5">
-                        <p style="color:#254345">{{ __('about_us.para_04') }}</p>
                     </div>
                 </div>
             </div>
@@ -40,28 +37,88 @@
                     <div class="col-md-12 text-center">
                         <h2 class="section-title font-alt mb-50 mt-30 ">Unrivaled Quality and Style</h2>
                     </div>
-                    <div class="col-sm-6 pr-md-5">
-
-                        <p style="color:#254345">Our extensive range of leather products, including shoes, wallets, and
-                            belts, are a testament to our unwavering commitment to quality and style.</p>
-                        <p style="color:#254345">We meticulously select the finest leathers and materials to ensure that
-                            every piece we create is a work of art</p>
-                        <p style="color:#254345">Each product embodies the essence of sophistication and showcases the
-                            uniqueness of genuine leather.
-                        </p>
+                    <div class="col-sm-12 pr-md-5">
+                        <div class="d-flex">
+                            <img src="{{ asset('frontend/images/milestone.png') }}" style="height: 26px;margin-right: 15px;"
+                                alt="logo">
+                            <p style="color:#254345">Our extensive range of leather products, including shoes, wallets, and
+                                belts, are a testament to our unwavering commitment to quality and style.</p>
+                        </div>
+                        <div class="d-flex">
+                            <img src="{{ asset('frontend/images/milestone.png') }}" style="height: 26px;margin-right: 15px;"
+                                alt="logo">
+                            <p style="color:#254345">We meticulously select the finest leathers and materials to ensure that
+                                every piece we create is a work of art</p>
+                        </div>
+                        <div class="d-flex">
+                            <img src="{{ asset('frontend/images/milestone.png') }}" style="height: 26px;margin-right: 15px;"
+                                alt="logo">
+                            <p style="color:#254345">Each product embodies the essence of sophistication and showcases the
+                                uniqueness of genuine leather.
+                            </p>
+                        </div>
                     </div>
-                    <div class="col-sm-6 pl-md-5">
+                </div>
+            </div>
+        </div>
+        <div class="container relative mt-40">
+            <h2 class="section-title font-alt mb-50 mt-50 mb-sm-40">Crafted with Care</h2>
+            <div class="section-text mb-50 mt-40 mb-sm-40">
+                <div class="row">
+                    <div class="col-sm-12 pr-md-5">
+                        <p style="color:#254345"> Handcrafted with care and attention to detail, our leather goods bear the
+                            mark of our skilled artisans. We celebrate the art of crafting by preserving age-old techniques
+                            and traditions while infusing a modern touch. The result? Products that are not just accessories
+                            but a statement of your individuality.
+                        </p>
 
-                        <div class="d-flex">
-                            <img src="{{ asset('frontend/images/milestone.png') }}" style="height: 26px;margin-right: 15px;"
-                                alt="logo">
-                            <p style="color:#254345">{{ __('about_us.para_4') }}</p>
-                        </div>
-                        <div class="d-flex">
-                            <img src="{{ asset('frontend/images/milestone.png') }}" style="height: 26px;margin-right: 15px;"
-                                alt="logo">
-                            <p style="color:#254345">{{ __('about_us.para_5') }}</p>
-                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="container relative mt-40">
+            <h2 class="section-title font-alt mb-50 mt-50 mb-sm-40">Customer Delight</h2>
+            <div class="section-text mb-50 mt-40 mb-sm-40">
+                <div class="row">
+                    <div class="col-sm-12 pr-md-5">
+                        <p style="color:#254345"> We are driven by the happiness of our customers. Our focus on providing
+                            exceptional customer experiences and the highest
+                            level of satisfaction has earned us the loyalty and trust of leather connoisseurs worldwide.
+
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container relative mt-40">
+            <h2 class="section-title font-alt mb-50 mt-50 mb-sm-40">Our Mission</h2>
+            <div class="section-text mb-50 mt-40 mb-sm-40">
+                <div class="row">
+                    <div class="col-sm-12 pr-md-5">
+                        <p style="color:#254345"> At Chapter 85, our mission is to continue pushing the boundaries of
+                            leather craftsmanship. We believe in creating
+                            products that stand out, products that become an essential part of your everyday life. Our
+                            commitment to quality,
+                            authenticity, and innovation is unwavering.
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container relative mt-40">
+            <h2 class="section-title font-alt mb-50 mt-50 mb-sm-40">Join Us on Our Journey</h2>
+            <div class="section-text mb-50 mt-40 mb-sm-40">
+                <div class="row">
+                    <div class="col-sm-12 pr-md-5">
+                        <p style="color:#254345"> Join us on this incredible journey as we craft the finest leather products
+                            and redefine what it means to own and cherish
+                            something truly exceptional. Thank you for being a part of our story.
+                        </p>
+
                     </div>
                 </div>
             </div>
